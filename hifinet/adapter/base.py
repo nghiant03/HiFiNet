c01423996715ba9d247071e7427a7da8da82ca12
Sinner <nghiant@mailbox.org>
2025-08-24 22:44:37 +0700

    refactor adaptor config to main config module

--- hifinet/adapter/base.py
+++ hifinet/adapter/base.py
@@ -1,18 +1,80 @@
 import pandas as pd
-from pathlib import Path
+import pandera.pandas as pa
+from loguru import logger
+from typing import Optional
 from abc import ABC, abstractmethod
-from pydantic_settings import BaseSettings
-from typing import Dict, Optional
+
+from hifinet.config import AdaptorConfig
 
 class BaseAdaptor(ABC):
-    def __init__(self, path: Path):
-        self.path = path
+    def __init__(self, config: AdaptorConfig):
+        self.config = config
+
     @abstractmethod
     def read(self) -> pd.DataFrame:
         pass
 
-class AdaptorConfig(BaseSettings):
-    path: Path
-    rename_columns: Optional[Dict[str, str]] = None
+    def process(self, data: pd.DataFrame) -> pd.DataFrame:
+        data = self._filter_by_id(data)
+        data = self._filter_by_period(data)
+        data = self._resample(data)
+        return data
+
+    def _filter_by_id(self, data: pd.DataFrame) -> pd.DataFrame:
+        if not self.config.subset_node:
+            return data
+
+        filter_data = data.copy()
+
+        filter_data = filter_data[filter_data["node_id"].isin(self.config.subset_node)].copy()
+        
+        return filter_data
+
+    def _filter_by_period(self, data: pd.DataFrame) -> pd.DataFrame:
+        if not self.config.period:
+            return data
+
+        filter_data = data.copy() 
+        
+        filter_data = filter_data[(filter_data["datetime"] >= self.config.period[0]) & (filter_data["datetime"] <= self.config.period[1])].copy()
+
+        return filter_data
+
+    def _resample(self, data: pd.DataFrame) -> pd.DataFrame:
+        if not self.config.resample_interval:
+            return data
+
+        resampled_list = []
+        unique_nodes = data["node_id"].unique()
+
+
+        for node in unique_nodes:
+            note_data = data[data["node_id"] == node].copy()
+            note_data = note_data.set_index("datetime")
+
+            nums_nan = note_data["target"].isna().sum()
+            logger.debug(f"Node {node}: Found {nums_nan} NaN values in target before resampling")
+            
+            resampled_mote = note_data.resample(self.config.resample_interval).mean()
+            resampled_mote["node_id"] = node
+            
+            nums_nan = resampled_mote["target"].isna().sum()
+
+            logger.debug(f"Node {node}: Found {nums_nan} NaN values in target after resampling, before interpolation")
+
+            resampled_mote["target"] = resampled_mote["target"].interpolate("time")
+
+            nums_nan = resampled_mote["target"].isna().sum()
+
+            logger.debug(f"Node {node}: Found {nums_nan} NaN values in target after interpolation")
+
+            resampled_mote = resampled_mote.reset_index()
+            resampled_list.append(resampled_mote)
+        
+        resampled_data = pd.concat(resampled_list, ignore_index=True)
+
+        return resampled_data
+
+
 
 
--- hifinet/config.py
+++ hifinet/config.py
@@ -1,4 +1,13 @@
-from pydantic_settings import BaseSettings
-from typing import Optional
+from datetime import datetime
+from pathlib import Path
+from typing import Dict, List
+from pydantic import BaseModel, Field, model_validator, field_validator
+from typing import Optional, Union
 
 
+class AdaptorConfig(BaseModel):
+    path: Path
+    period: Optional[List[datetime]] = None
+    subset_node: Optional[List[int]] = None
+    resample_interval: Optional[str] = None
+    rename_columns: Optional[Dict[str, str]] = None
