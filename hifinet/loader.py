c4b1621f51eb81a9e766a4564bd18682a5ed6ce0
Sinner <nghiant@mailbox.org>
2025-08-25 16:55:57 +0700

    add functionality to process custom dataset

--- hifinet/loader.py
+++ hifinet/loader.py
@@ -1,5 +1,10 @@
-from loguru import logger
+from pathlib import Path
 
+import pandas as pd
+from loguru import logger
+from pandera.errors import SchemaErrors
+
+from hifinet.adapter.base import DataSchema
 from hifinet.adapter.intel import IntelAdaptor
 
 ADAPTORS = {"intel": IntelAdaptor}
@@ -11,6 +16,16 @@ def load_data(dataset: str):
     if dataset in ADAPTORS:
         logger.info(f"Using default {dataset} adapter.")
         adaptor = ADAPTORS[dataset]()
+        data = adaptor.read()
+    else:
+        dataset_path = Path(dataset)
+        logger.info(f"Loading raw dataset from {dataset_path}")
+        data = pd.read_csv(dataset_path)
 
-    data = adaptor.read()
+        try:
+            data = DataSchema.validate(data, lazy=True)
+        except SchemaErrors:
+            logger.error("Failed to validated data")
+            raise
+
     return data
