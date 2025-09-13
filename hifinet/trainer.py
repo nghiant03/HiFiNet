d5099c6dad1f308b9e3d94a827037441aa540f83
Sinner <nghiant@mailbox.org>
2025-09-13 16:46:30 +0700

    add temp variable

--- hifinet/trainer.py
+++ hifinet/trainer.py
@@ -1,19 +1,82 @@
+import re
 from typing import Any
 
 import pandas as pd
 from loguru import logger
 
-from hifinet.model.random_forest import RandomForestModel
+from hifinet.model import RandomForestModel
 
 
 class Trainer:
-    def __init__(self, data: pd.DataFrame, ratio):
+    def __init__(self, data: pd.DataFrame, ratio: float, temp):
         self.data = data
         self.ratio = ratio
+        self.sequence_columns = [
+            column for column in self.data.columns if re.match(r"feature_*", column)
+        ]
+        self.sequence_columns.append("target")
+        self.train_data, self.val_data, self.test_data = self._split(temp)
 
-    def train(self, model_name: str, model_params: dict[str, Any]):
+    def train(self, model_name: str, model_params: dict[str, Any] | None = None):
         match model_name:
             case "random_forest":
-                model = RandomForestModel(**model_params)
+                if model_params:
+                    self.model = RandomForestModel(
+                        sequence_columns=self.sequence_columns, **model_params
+                    )
+                else:
+                    self.model = RandomForestModel(
+                        sequence_columns=self.sequence_columns
+                    )
             case _:
                 logger.error(f"Model {model_name} not implemented")
+                raise NotImplementedError
+
+        x = self.train_data[self.sequence_columns]
+        y = self.train_data["type"]
+
+        self.model.fit(x, y)
+
+    def _split(self, temp):
+        match temp:
+            case 0:
+                return (
+                    self.data[self.data["datetime"] < "2023-08-01"],
+                    self.data[
+                        (self.data["datetime"] >= "2023-08-01")
+                        | (self.data["datetime"] < "2023-10-01")
+                    ],
+                    self.data[
+                        (self.data["datetime"] >= "2023-10-01")
+                        | (self.data["datetime"] < "2023-11-01")
+                    ],
+                )
+            case 1:
+                return (
+                    self.data[
+                        (self.data["datetime"] >= "2023-02-01")
+                        | (self.data["datetime"] < "2023-09-01")
+                    ],
+                    self.data[
+                        (self.data["datetime"] >= "2023-09-01")
+                        | (self.data["datetime"] < "2023-11-01")
+                    ],
+                    self.data[
+                        (self.data["datetime"] >= "2023-11-01")
+                        | (self.data["datetime"] < "2023-12-01")
+                    ],
+                )
+            case 2:
+                return (
+                    self.data[
+                        (self.data["datetime"] >= "2023-03-01")
+                        | (self.data["datetime"] < "2023-10-01")
+                    ],
+                    self.data[
+                        (self.data["datetime"] >= "2023-10-01")
+                        | (self.data["datetime"] < "2023-12-01")
+                    ],
+                    self.data[self.data["datetime"] >= "2023-12-03"],
+                )
+            case _:
+                raise NotImplementedError
